6da71be7a96b9cb46863a67f3af7d2eb6e290e18
sainu <saino.katsutoshi@moneyforward.co.jp>
2021-01-17 21:22:16 +0900

    Copy blockchain.ts from 1-sainu-coin

---
+++ 2-sainu-coin-with-web-view/src/lib/blockchain.ts
@@ -0,0 +1,161 @@
+import { SHA256 } from 'crypto-js';
+import { ec as EC } from 'elliptic';
+const ec = new EC('secp256k1');
+
+export class Transaction {
+  private signature?: string;
+
+  constructor(public fromAddress: string | null, public toAddress: string, public amount: number) {}
+
+  private calculateHash(): string {
+    return SHA256(this.fromAddress + this.toAddress + this.amount).toString();
+  }
+
+  signTransaction(signingKey: EC.KeyPair): void {
+    if (signingKey.getPublic('hex') !== this.fromAddress) {
+      throw new Error('You cannot sign transactions for other wallets!');
+    }
+
+    const hashTx = this.calculateHash();
+    const sig = signingKey.sign(hashTx, 'base64');
+    this.signature = sig.toDER('hex');
+  }
+
+  isValid(): boolean {
+    if (this.fromAddress === null) return true;
+
+    if (!this.signature || this.signature.length === 0) {
+      throw new Error('No signature in this transaction');
+    }
+
+    const publicKey = ec.keyFromPublic(this.fromAddress, 'hex');
+    return publicKey.verify(this.calculateHash(), this.signature);
+  }
+}
+
+class Block {
+  private timestamp: number;
+  public transactions: Transaction[];
+  public previousHash: string;
+  public hash: string;
+  private nonce: number;
+
+  constructor(timestamp: number, transactions: Transaction[], previousHash = '') {
+    this.timestamp = timestamp;
+    this.transactions = transactions;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+    this.nonce = 0;
+  }
+
+  calculateHash(): string {
+    return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
+  }
+
+  mineBlock(difficulty: number) {
+    while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+
+    console.log("Block mined: " + this.hash);
+  }
+
+  hasValidTransactions() {
+    for (const tx of this.transactions) {
+      if (!tx.isValid()) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+export class BlockChain {
+  public chain: Block[];
+  private difficulty: number;
+  private pendingTransactions: Transaction[];
+  private miningReward: number;
+
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+    this.difficulty = 2;
+    this.pendingTransactions = [];
+    this.miningReward = 100;
+  }
+
+  private createGenesisBlock(): Block {
+    return new Block(Number(new Date('2017/01/01')), [], "0");
+  }
+
+  private getLatestBlock(): Block {
+    return this.chain[this.chain.length - 1];
+  }
+
+  minePendingTransactions(miningRewardAddress: string): void {
+    const rewardTx = new Transaction(null, miningRewardAddress, this.miningReward);
+    this.pendingTransactions.push(rewardTx);
+
+    const block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
+    block.mineBlock(this.difficulty);
+
+    console.log('Block successfully mined!');
+    this.chain.push(block);
+
+    this.pendingTransactions = [
+      new Transaction(null, miningRewardAddress, this.miningReward)
+    ];
+  }
+
+  addTransaction(transaction: Transaction): void {
+    if (!transaction.fromAddress || !transaction.toAddress) {
+      throw new Error('Transaction must include from and to address');
+    }
+
+    if (!transaction.isValid()) {
+      throw new Error('Cannot add invalid transaction to chain');
+    }
+
+    this.pendingTransactions.push(transaction);
+  }
+
+  getBalanceOfAddress(address: string): number {
+    let balance = 0;
+
+    for (const block of this.chain) {
+      for (const trans of block.transactions) {
+        if (trans.fromAddress == address) {
+          balance -= trans.amount;
+        }
+
+        if (trans.toAddress == address) {
+          balance += trans.amount;
+        }
+      }
+    }
+
+    return balance;
+  }
+
+  isChainValid(): boolean {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (!currentBlock.hasValidTransactions()) {
+        return false;
+      }
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
